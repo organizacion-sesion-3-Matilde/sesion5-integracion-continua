f09ca659950826c7bd3b57e0b11341df16e5e398
matigimeno <104157885+matigimeno@users.noreply.github.com>
2022-05-03 13:05:40 +0200

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,12 @@
+# En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
+def suma(x,y):
+
+    # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
+    return x + y
+
+def espar(x):
+    if a % 2 == 0:
+    return True
+else:
+    return False
+
